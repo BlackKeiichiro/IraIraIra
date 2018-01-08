3adfc880f09415a1fe126dd36756e91b4759f964
BLACK <blackk16.k@gmail.com>
2018-01-09 06:18:13 +0900

    First Commit(the commit as unity project)

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+	private Vector3 mouse_position,save_position;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnMouseDown(){
+		mouse_position =  Camera.main.WorldToScreenPoint(this.transform.position);
+		save_position = this.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+		save_position.z = this.transform.position.z;
+	}
+
+	void OnMouseDrag(){
+		Vector3 current_point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mouse_position.z);
+		Vector3 current_position = Camera.main.ScreenToWorldPoint(current_point) + save_position;
+		current_position.z = this.transform.position.z;
+		this.transform.position = current_position;
+	}
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : SingletonMonoBehaviour <GameManager>{
+	public uint player_point = 0;
+	public string player_name = "";
+
+	protected void GetName(){
+		
+	}
+}
